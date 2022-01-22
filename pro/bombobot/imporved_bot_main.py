8c6b58803b884838abac9f87275e9d970e124c12
VocanicZ <35866098+VocanicZ@users.noreply.github.com>
2022-01-22 18:42:15 +0700

    z

--- pro/bombobot/imporved_bot_main.py
+++ pro/bombobot/imporved_bot_main.py
@@ -1,6 +1,11 @@
+import cv2 as cv
 from src.ai import *
 import ctypes
 import math
+import numpy as  np
+import winsound
+from src.imgbot import ison_curscreen
+from src.fl import get_img_path
 
 key_pos = {'back':(0, 0), 'map': (0, 0), 'hero':(0, 0), 'all':(0, 0), 'close':(0, 0)}
 
@@ -17,7 +22,12 @@ def conv(key, scr):
 
 def remap(scr):
         return
-        
+
+def output_screen_pos():
+        img = cv.cvtColor(np.array(gui.screenshot()), cv.COLOR_RGB2BGR)
+        while 1:
+                cv.imshow('screenshot', img)
+
 def main():
         global width, hight, screensize
         
@@ -28,6 +38,47 @@ def main():
         pos = int(width)+(2880*(screen-1))+(100 if bar == 4 or bar == 6 else 0), int(hight)+(100 if bar == 2 or bar == 8 else 0)
         print(pos)
         moveto(pos)
-#cur_pos()   #(5760, 1620)
+
+def increment(data, x=0, y=0):
+        data = list(data)
+        data[0] += x
+        data[1] += y
+        return tuple(data)
+        
+def dof(n, data, nx=0,ny=0):
+        for k in n:
+                if isinstance(k, int):
+                        if k == 0:
+                                click()
+                        elif k == 1:
+                                sleep(1)
+                        elif isinstance(k, str):
+                                click(increment(data[k]), x=nx, y=ny)
+                if ison_curscreen(get_img_path('disconnect')):
+                        winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
+
+def temporary_main():
+        dis_x = 619
+        dis_y = 360
+        dis_num = 3
+        data = {'back': (0,0), 'map': (0,0), 'hero': (0,0), 'work': (0,0), 'close': (0,0)}
+        script = [['close', 'back', 'map'], ['hero', 0, 'work', 1, 'close']]
+        remap = 4
+        cycle = 1000
+        for i in range(1, cycle):
+                for n in range(dis_num):
+                        if n < 3:
+                                x = n*dis_x
+                        y = 0
+                        if i*remap%40 == 0:
+                                dof(script[1], data, nx=x, ny=y)
+                        dof(script[0], data, nx=x, ny=y)
+                sleep(mtos(remap))
+
+cur_pos()   #(5760, 1620)
+#output_screen_pos()
+
+
 if __name__ == '__main__':
-        main()
+        temporary_main()
+        #main()
